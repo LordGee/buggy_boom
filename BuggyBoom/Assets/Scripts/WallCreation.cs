1e10b5251d77b0671860a6099eb5680357aeb3ac
LordGee <gordon.johnson@mychaos.co.uk>
2017-10-07 13:49:12 +0100

    Generating Walls
    + Added Wall Spawner left and right to generate random building
    + Added script the generate at specific intervals and child object to the spawner
    + Added script to create wall with random size and add translation
    + Added a shredder object behind the camera to destroy any ojects that go out of view.
    
    // todo: add random material to the creation script

---
+++ BuggyBoom/Assets/Scripts/WallCreation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCreation : MonoBehaviour {
+
+    public float wallSpeed = 5f;
+
+    private float scaleX, scaleY, scaleZ;
+    private Color col;
+
+    // Use this for initialization
+    void Start () {
+        GetRandomSize();
+        transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0f, 0f, -(wallSpeed * Time.deltaTime));
+	}
+
+    private void GetRandomSize()
+    {
+        scaleX = Random.Range(2f, 8f);
+        scaleY = Random.Range(2f, 10f);
+        scaleZ = Random.Range(2f, 5f);
+    }
+}
---
+++ BuggyBoom/Assets/Scripts/WallSpawning.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawning : MonoBehaviour
+{
+
+    public GameObject wall;
+    public float timeSinceLastSpawn = 0f;
+    public float spawnInterval = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Time.timeSinceLevelLoad - timeSinceLastSpawn > spawnInterval)
+	    {
+	        Vector3 pos = transform.position;
+	        GameObject newWall = Instantiate(wall, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
+	        newWall.transform.parent = gameObject.transform;
+	        timeSinceLastSpawn = Time.timeSinceLevelLoad;
+	    }
+	}
+}
