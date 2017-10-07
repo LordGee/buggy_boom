1ee5db601578b9ce1f4c731a773480228d5c0c66
LordGee <gordon.johnson@mychaos.co.uk>
2017-10-07 22:37:11 +0100

    Spawn Enermy Cars
    + Import asset 3D model car, credit to http://u3d.as/8Er
    + Added a spawner to generate game in random lanes
    + After a few attempts, pre-defined the lanes by grid reference in an array
    + Added rigidbody and coliders to all cars inc player
    + Import skybox for background, credit to http://u3d.as/mZv
    + Changed camera position
    
    TODO:
    - Attach camera to car to follow X movement,
    - Implement projectiles to clear path through other cars,
    - add alternative obstacles

---
+++ BuggyBoom/Assets/Scripts/NPCObstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCObstacle : MonoBehaviour {
+
+    //Public Variables
+    public float obstacleCurrentSpeed = 10f;  
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, 0f, -(obstacleCurrentSpeed * Time.deltaTime));
+    }
+}
---
+++ BuggyBoom/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    // Public Variables
+    [Tooltip("Attach Prefab of cube with appropriate Rigidbody and COnstraints")] public GameObject carObstacle;
+
+    [Tooltip("Define how often a Wall is Spawned")] public float spawnInterval = 1f;
+    [Tooltip("Define time until the first object is spawned")] public float timeBeforeFirstSpawn = 15f;
+    public int[] laneArray = {-3, -1, 1, 3};
+
+    // Private Variables
+    private float timeSinceLastSpawn = 0f;
+    private bool startSpawning = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startSpawning && Time.timeSinceLevelLoad > timeBeforeFirstSpawn)
+        {
+            startSpawning = true;
+        }
+        if (startSpawning && Time.timeSinceLevelLoad - timeSinceLastSpawn > spawnInterval)
+        {
+            GameObject newObstacle = Instantiate(
+                carObstacle, 
+                new Vector3(
+                    laneArray[Random.Range(0, laneArray.Length)], 
+                    transform.position.y, 
+                    transform.position.z), 
+                Quaternion.identity);
+            newObstacle.transform.parent = gameObject.transform;
+            timeSinceLastSpawn = Time.timeSinceLevelLoad;
+        }
+    }
+}
