647470be4f7804f304d09bc7f543bd0e7feddc63
LordGee <gordon.johnson@mychaos.co.uk>
2017-10-07 11:21:06 +0100

    Terrain - Main Road
    + Obtained road surface and side walk images (Credit to texturelib.com for free use of the textures)
    + Edited Image to to include side path on both sides
    + Created cude for terrain
    + Added new image as material and set tiling aspects
    + Created a MaterialOffset script to provide acceleration of the road which maintains a certain speed at threshold.

---
+++ BuggyBoom/Assets/Scripts/MaterialOffset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialOffset : MonoBehaviour
+{
+    // Public Variables
+    [Tooltip("Define the starting speed of the material")]
+    public float offsetSpeed = 0.1f;
+    [Tooltip("Define the theshold of the acceleration period")]
+    public float initialThreshold = 2.4f;
+    [Tooltip("Define the acceleration rate prior to reaching threshold")]
+    public float preIncrementSpeed = 0.001f;
+
+    // Private Variables
+    private Renderer renderer;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.SetTextureOffset("_MainTex", new Vector2(0f, -(Time.time * offsetSpeed)));
+	    if (offsetSpeed < initialThreshold)
+	    {
+	        offsetSpeed += preIncrementSpeed;
+	    }
+	}
+}
