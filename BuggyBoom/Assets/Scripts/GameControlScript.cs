dc140843c610e74f9c34fd4b8573ec130b61a353
LordGee <gordon.johnson@mychaos.co.uk>
2017-10-11 22:35:20 +0100

    Game Contrroller
    + Added game controller to manage major variables within the game
    + Getters and setters for all attributes
    + Added progression element that increases the games difficulty over a period of time, but at the same time increases the score multiplier.
    + Speed maxes out to ensure possible gameplay
    + Added damage to player and npc function that will apply the correct solution and provide a central location for this to take place.
    + Update NPC Jeep spawning script to grab the relevent uptodate values from the game controller and apply to newly created NPC objects

---
+++ BuggyBoom/Assets/Scripts/GameControlScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlScript : MonoBehaviour
+{
+
+    public float playerHealth = 100f;
+    public float playerDamage = 10f;
+
+    public float npcJeepHealth = 10f;
+    public float npcJeepDamage = 10f;
+    public float npcJeepSpeed = 10f;
+
+    private float playerScore, playerMultipler, playerPoints;
+    private float progressionTimer, progressionCountdown, progressionIncrementer;
+    private const float maxSpeed = 30f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    playerScore = 0f;
+	    playerMultipler = 1f;
+	    playerPoints = 100f;
+	    progressionTimer = Time.timeSinceLevelLoad;
+	    progressionIncrementer = 0.1f;
+	    progressionCountdown = 12f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Time.timeSinceLevelLoad - progressionTimer > progressionCountdown)
+	    {
+            ProgressionCalc(ref npcJeepHealth);
+	        ProgressionCalc(ref playerMultipler);
+	        ProgressionCalc(ref progressionCountdown);
+	        if (npcJeepSpeed <= maxSpeed)
+	        {
+	            ProgressionCalc(ref npcJeepSpeed);
+	        }
+	        progressionTimer = Time.timeSinceLevelLoad;
+        }
+	}
+
+     public void DamagePlayer(float _dmg)
+    {
+        playerHealth -= _dmg;
+        if (playerHealth <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void DamageNPC(GameObject _obj, float _dmg, ref float _hea)
+    {
+        _hea -= _dmg;
+        if (_hea <= 0)
+        {
+            AddPoints(playerScore);
+            Destroy(_obj);
+        }
+    }
+
+    public void AddPoints(float _pts)
+    {
+        playerScore += _pts * playerMultipler;
+    }
+
+    private void ProgressionCalc(ref float _value)
+    {
+        _value += _value * progressionIncrementer;
+    }
+
+    public float GetPlayerHealth() { return playerHealth; }
+    public float GetPlayerDamage() { return playerDamage; }
+
+    public float GetNpcJeepHealth() { return npcJeepHealth; }
+    public float GetNpcJeepDamage() { return npcJeepDamage; }
+    public float GetNpcJeepSpeed() { return npcJeepSpeed; }
+
+    void GameOver()
+    {
+        Debug.LogError("You Broke the Game by DYING!");
+    }
+}
--- BuggyBoom/Assets/Scripts/NPCObstacle.cs
+++ BuggyBoom/Assets/Scripts/NPCObstacle.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class NPCObstacle : MonoBehaviour {
 
     //Public Variables
-    [Tooltip("The speed of the obstacle, moving towards the player")]
-    public float obstacleCurrentSpeed = 10f;
+    
     [Tooltip("Gameobject instantiated and provides effects when the object collides with the player")]
     public GameObject DeathParticleEffect;
+
+    // Private Variables
+    private GameControlScript gameControl;
+    private float npcHealth;
+    private float npcDamage;
+    private float npcSpeed;
+
+    void Start()
+    {
+        gameControl = FindObjectOfType<GameControlScript>();
+        npcHealth = gameControl.GetNpcJeepHealth();
+        npcDamage = gameControl.GetNpcJeepDamage();
+        npcSpeed = gameControl.GetNpcJeepSpeed();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0f, 0f, -(obstacleCurrentSpeed * Time.deltaTime));
+        print(npcSpeed);
+        transform.Translate(0f, 0f, -(npcSpeed * Time.deltaTime));
     }
 
     void OnCollisionEnter(Collision col)
@@ -22,9 +36,13 @@ public class NPCObstacle : MonoBehaviour {
         {
             GameObject death = Instantiate(DeathParticleEffect, transform.position, Quaternion.identity);
             Destroy(death, 2f);
+            gameControl.DamagePlayer(npcDamage);
+            gameControl.AddPoints(npcSpeed);
             Destroy(this.gameObject, 0.1f); // added this small amount of time makes such a difference
         }
+        else if (col.gameObject.tag == "Projectile")
+        {
+            gameControl.DamageNPC(this.gameObject, 10f, ref npcHealth);
+        }
     }
-
-
 }
