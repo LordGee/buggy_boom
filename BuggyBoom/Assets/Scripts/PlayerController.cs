befe86497f06fd18281c331fd2ba9b70524e87e7
LordGee <gordon.johnson@mychaos.co.uk>
2017-10-16 10:00:42 +0100

    Minor cleanup

--- BuggyBoom/Assets/Scripts/PlayerController.cs
+++ BuggyBoom/Assets/Scripts/PlayerController.cs
@@ -75,7 +75,6 @@ public class PlayerController : MonoBehaviour
 	                FireProjectile();
 	                projectileCounter = 0;
 	            }
-	            print(projectileCounter);
 	        }
         }
 	        
